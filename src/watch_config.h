8ee9f7b03f0352a1a8bf93d0aa19a4744d302c4e
clach04 <Chris.Clark@actian.com>
2016-03-19 14:16:59 -0700

    config cleanup

--- src/watch_config.h
+++ src/watch_config.h
@@ -17,29 +17,9 @@
 
 #define BG_IMAGE RESOURCE_ID_IMAGE_SPAWN
 //#define BG_IMAGE_GRECT GRectZero
-#define BG_IMAGE_GRECT GRect((144 - 132) / 2, 0, 132, 132)  // use same size as image. On Aplite can determine this at runtime but not Basalt
+#define BG_IMAGE_GRECT GRect((144 - 120) / 2, 0, 120, 120)  // use same size as image. On Aplite can determine this at runtime but not Basalt
 
-#ifdef  NO_DATE
-    #define CLOCK_POS GRect(0, 118, 144, 168) /* bottom of screen for FONT_KEY_ROBOTO_BOLD_SUBSET_49 with one pixel border on bottom */
-
-    //#define FONT_SYSTEM_NAME FONT_KEY_BITHAM_42_BOLD
-    //#define CLOCK_POS GRect(0, 125, 144, 168) /* bottom of screen for FONT_KEY_BITHAM_42_BOLD  with one pixel border on bottom */
-
-    //#define FONT_SYSTEM_NAME FONT_-KEY_ROBOTO_CONDENSED_21
-    //#define CLOCK_POS GRect(0, 142, 144, 168) /* bottom of screen for FONT_KEY_ROBOTO_CONDENSED_21 */
-
-    //#define FONT_SYSTEM_NAME FONT_KEY_GOTHIC_28_BOLD
-    //#define CLOCK_POS GRect(0, 138, 144, 168) /* bottom of screen for FONT_KEY_GOTHIC_28_BOLD */
-
-    //#define FONT_SYSTEM_NAME FONT_KEY_BITHAM_30_BLACK /* works well if 144x144 image is used above, little wasted space */
-    //#define CLOCK_POS GRect(0, 136, 144, 168) /* bottom of screen for FONT_KEY_BITHAM_30_BLACK */
-#else
-//#define CLOCK_POS GRect(0, -10, 144, 168) /* Top of screen */
-//#define CLOCK_POS GRect(0, 10, 144, 168) /* near top of screen */
-//#define CLOCK_POS GRect(0, 52, 144, 168) /* Center of screen */
-//#define CLOCK_POS GRect(0, 58, 144, 168) /* Center logo if logo is top aligned */
-#define CLOCK_POS GRect(0, 75, 144, 168) /* if logo is top aligned */
-#endif
+#define CLOCK_POS GRect(0, 118, 144, 168) /* bottom of screen for FONT_KEY_ROBOTO_BOLD_SUBSET_49 with one pixel border on bottom */
 
 //#define BT_POS GRect(0, 40, 144, 168) /* probably taller than really needed */
 #define BT_POS GRect(0, 50, 144, 168) /* probably taller than really needed */
