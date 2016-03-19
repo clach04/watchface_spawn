c5b76a8d1ed3430dedace87a2ae4bbb503db5745
clach04 <Chris.Clark@actian.com>
2016-03-19 14:29:27 -0700

    add battery and date

--- src/watch_config.h
+++ src/watch_config.h
@@ -12,19 +12,32 @@
 
 #define FONT_BT_SYSTEM_NAME FONT_KEY_GOTHIC_18_BOLD
 //#define NO_BLUETOOTH /* Do not show bluetooth disconnect status */
-#define NO_BATTERY /* Do not show battery status */
-#define NO_DATE /* Do not show date */
+
+//#define NO_BATTERY /* Do not show battery status */
+#define BAT_FMT_STR "%d%%"
+//#define DRAW_BATTERY
+
+#define DATE_FMT_STR "%a\n%b\n%d"  /* TODO review %d for day */
+#define MAX_DATE_STR "Thu\n00\nAug"  /* if custom version of DATE_FMT_STR is set, MAX_DATE_STR  needs to be updated too */
+//#define NO_DATE
 
 #define BG_IMAGE RESOURCE_ID_IMAGE_SPAWN
-//#define BG_IMAGE_GRECT GRectZero
-#define BG_IMAGE_GRECT GRect((144 - 120) / 2, 0, 120, 120)  // use same size as image. On Aplite can determine this at runtime but not Basalt
+/* PBL_RECT 144x168*/
+#define IMAGE_RES_X 120  /* NOTE if image res changes - this needs to change too! */
+#define IMAGE_RES_Y 120  /* NOTE if image res changes - this needs to change too! */
+/* Image shifted down from top of screen, centered horizontally */
+#define BG_IMAGE_GRECT GRect((144 - IMAGE_RES_X) / 2, 10,  IMAGE_RES_X, IMAGE_RES_Y)
 
 #define CLOCK_POS GRect(0, 118, 144, 168) /* bottom of screen for FONT_KEY_ROBOTO_BOLD_SUBSET_49 with one pixel border on bottom */
 
-//#define BT_POS GRect(0, 40, 144, 168) /* probably taller than really needed */
 #define BT_POS GRect(0, 50, 144, 168) /* probably taller than really needed */
-#define DATE_POS GRect(0, 140, 144, 168) /* probably taller than really needed */
-#define BAT_POS GRect(0, 140, 144, 168) /* probably taller than really needed */
+
+#define DATE_POS GRect(0, -5, 144, 168) /* probably taller than really needed */
+#ifdef DRAW_BATTERY
+    #define BAT_POS GRect(8, 8, 144, 168)
+#else
+    #define BAT_POS GRect(8, 0, 144, 168) /* probably taller than really needed */
+#endif /* DRAW_BATTERY */
 
 
 /* for screen shots and font testing
