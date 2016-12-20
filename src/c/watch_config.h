7c59b3029dc04695f27428374c0d906606e2ca48
clach04 <Chris.Clark@actian.com>
2016-12-19 18:10:54 -0800

    Added quiet time icon
    By Franciszek Zygmunt (Averso) https://raw.githubusercontent.com/Averso/HotlineMiamiFace/master/resources/images/qt.png commit 6e2f2ce77f200a24dc48e92124b597669744a5d8

--- src/c/watch_config.h
+++ src/c/watch_config.h
@@ -14,18 +14,21 @@
 //#define NO_BLUETOOTH /* Do not show bluetooth disconnect status */
 
 //#define NO_BATTERY /* Do not show battery status */
-#define BAT_FMT_STR "%d%%"
 //#define DRAW_BATTERY
 
 #define DATE_FMT_STR "%a\n%b\n%d"  /* TODO review %d for day */
 #define MAX_DATE_STR "Thu\n00\nAug"  /* if custom version of DATE_FMT_STR is set, MAX_DATE_STR  needs to be updated too */
 //#define NO_DATE
 
+#define QUIET_TIME_IMAGE RESOURCE_ID_IMAGE_QUIET_TIME
+#define QUIET_TIME_IMAGE_GRECT GRect(3, 60, 14, 14)  // 14x14 image
+
 #define BG_IMAGE RESOURCE_ID_IMAGE_SPAWN
 /* PBL_RECT 144x168*/
 #define IMAGE_RES_X 120  /* NOTE if image res changes - this needs to change too! */
 #define IMAGE_RES_Y 120  /* NOTE if image res changes - this needs to change too! */
-/* Image shifted down from top of screen, centered horizontally */
+
+/* Image a bottom of screen, centered horizontally */
 #define BG_IMAGE_GRECT GRect((144 - IMAGE_RES_X) / 2, 168 - IMAGE_RES_Y,  IMAGE_RES_X, IMAGE_RES_Y)
 
 #define CLOCK_POS GRect(0, -15, 144, 168) /* top of screen for FONT_KEY_ROBOTO_BOLD_SUBSET_49 */
@@ -36,6 +39,7 @@
 #ifdef DRAW_BATTERY
     #define BAT_POS GRect(8, 43, 144, 168)
 #else
+    #define BAT_FMT_STR "%d%%"
     #define BAT_POS GRect(8, 30, 144, 168) /* probably taller than really needed */
 #endif /* DRAW_BATTERY */
 
